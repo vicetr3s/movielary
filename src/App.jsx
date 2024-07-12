fdfffd4d55139e570ade87d955b335673e8aecb8
vicetr3s <vicediazcastex@gmail.com>
2024-07-12 10:38:24 -0400

    feat: added SearchBar.jsx
    installed a plugin to handle SVGs easily

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css"
 import Header from "./components/Header"
 import PageTitle from "./components/PageTitle.jsx";
+import SearchBar from "./components/SearchBar.jsx";
 
 export default function App() {
     return (
         <>
             <Header/>
             <PageTitle/>
+            <SearchBar/>
         </>
     )
 }
