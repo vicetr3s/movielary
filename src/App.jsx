bd474a840f91681003f164fc65b8c11385f91d3f
vicetr3s <vicediazcastex@gmail.com>
2024-07-15 16:00:08 -0400

    feat: progress in movie cards component
    added MovieCard.jsx
    added MovieCardsContainer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css"
 import Header from "./components/Header"
 import PageTitle from "./components/PageTitle.jsx";
 import SearchAndDiscover from "./components/SearchAndDiscover.jsx";
+import MovieCardsContainer from "./components/MovieCardsContainer.jsx";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Header/>
             <PageTitle/>
             <SearchAndDiscover/>
+            <MovieCardsContainer/>
         </main>
     )
 }
