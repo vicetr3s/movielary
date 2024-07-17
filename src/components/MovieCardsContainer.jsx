8333680d7946e4433f36794f840ef6dea5c2aa65
vicetr3s <vicediazcastex@gmail.com>
2024-07-16 22:03:11 -0400

    feat: progress in movie cards and its container
    potential problem when description text is too long, it will be fixed

--- src/components/MovieCardsContainer.jsx
+++ src/components/MovieCardsContainer.jsx
@@ -9,7 +9,26 @@ export default function MovieCardsContainer() {
                 imgUrl={"https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg"}
                 description={"An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."}
                 title={"Fight Club"}
-                tags={{imdb: "10 / 10", year: "1999", genre: "Drama"}}/>
+                tags={{imdb: "10 / 10", year: "1999", genre: "Drama"}}
+            />
+            <MovieCard
+                imgUrl={"https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg"}
+                description={"Un texto mas largo eooooo eoooo eoooo eoooooooo eoooo eoooooooooooooooo  eeeeeeeeeeeeeeeeeeAn insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."}
+                title={"Fight Club"}
+                tags={{imdb: "10 / 10", year: "1999", genre: "Drama"}}
+            />
+            <MovieCard
+                imgUrl={"https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg"}
+                description={"An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."}
+                title={"Fight Club"}
+                tags={{imdb: "10 / 10", year: "1999", genre: "Drama"}}
+            />
+            <MovieCard
+                imgUrl={"https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg"}
+                description={"An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."}
+                title={"Fight Club"}
+                tags={{imdb: "10 / 10", year: "1999", genre: "Drama"}}
+            />
         </section>
     )
 }
