d2aab5e877de0ac98df024919c2808d14474851d
vicetr3s <vicediazcastex@gmail.com>
2024-08-03 12:37:52 -0400

    feat: finished similar movies section
    added a react-slick carousel

---
+++ src/components/movies/MoviesCarousel.jsx
@@ -0,0 +1,29 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import {NavLink} from "react-router-dom";
+
+export default function MoviesCarousel({movies}) {
+    const responsive = [{breakpoint: 1280, settings: {slidesToShow: 3}},
+        {breakpoint: 900, settings: {slidesToShow: 2}}, {breakpoint: 600, settings: {slidesToShow: 1}},
+    ];
+    const settings = {
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: responsive,
+    }
+
+    return (
+        <Slider {...settings} className={"movie-suggestions-container container"}>
+            {movies && movies?.map(movie => (
+                <NavLink to="/movie" state={{id: movie.id}} className={"movie-suggestion"}>
+                    <img src={movie.imgUrl} alt={movie.title}/>
+                </NavLink>
+            ))}
+        </Slider>
+    )
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,7 @@
 import CreatorImage from "../components/about/CreatorImage.jsx";
 import CreatorDescription from "../components/about/CreatorDescription.jsx";
-import {useEffect} from "react";
-import {getWordDefinition} from "../lib/dictionary-api.js";
 
 export default function About() {
-
-    useEffect(() => {
-        getWordDefinition("ace").then(wordDefinition => {console.log(wordDefinition);});
-    }, []);
     return (
         <main className="about-main grid-spacing">
             <CreatorDescription/>
